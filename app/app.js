57c39ab60f738a227e7bf7e84129f8fed3046475
santiaago <santiago.ariassar@gmail.com>
2015-05-23 17:11:22 +0100

    add hello world app

---
+++ app/app.js
@@ -0,0 +1,30 @@
+if (typeof __decorate !== "function") __decorate = function (decorators, target, key, desc) {
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") return Reflect.decorate(decorators, target, key, desc);
+    switch (arguments.length) {
+        case 2: return decorators.reduceRight(function(o, d) { return (d && d(o)) || o; }, target);
+        case 3: return decorators.reduceRight(function(o, d) { return (d && d(target, key)), void 0; }, void 0);
+        case 4: return decorators.reduceRight(function(o, d) { return (d && d(target, key, o)) || o; }, desc);
+    }
+};
+if (typeof __metadata !== "function") __metadata = function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+/// <reference path="typings/angular2/angular2.d.ts" />
+var angular2_1 = require('angular2/angular2');
+// Annotation section
+var PomotimeComponent = (function () {
+    function PomotimeComponent() {
+        this.name = 'Pomoti.me';
+    }
+    PomotimeComponent = __decorate([
+        angular2_1.Component({
+            selector: 'pomotime-app'
+        }),
+        angular2_1.View({
+            template: '<h1>Hello {{ name }}</h1>'
+        }), 
+        __metadata('design:paramtypes', [])
+    ], PomotimeComponent);
+    return PomotimeComponent;
+})();
+angular2_1.bootstrap(PomotimeComponent);
