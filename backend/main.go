9244dded8cbbebce99acd305bc8fd1f1de7d2222
santiaago <santiago.ariassar@gmail.com>
2015-05-24 13:23:29 +0100

    simple api/helloworld route

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"log"
 	"net/http"
@@ -18,6 +19,8 @@ func init() {
 func main() {
 	r := new(route.Router)
 
+	r.HandleFunc("/api/hello", helloWorld)
+
 	r.AddStaticResource(root)
 
 	log.Println("Listening on " + os.Getenv("PORT"))
@@ -25,4 +28,23 @@ func main() {
 	if err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
+}
+
+// helloWorld handler returns a json file with a helloworld message.
+//
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	data := struct {
+		Message string
+	}{
+		"hello world",
+	}
+
+	if err := renderJson(w, data); err != nil {
+		log.Println(err)
+	}
+}
+
+// renderJson renders data to json and writes it to response writer
+func renderJson(w http.ResponseWriter, data interface{}) error {
+	return json.NewEncoder(w).Encode(data)
 }
