c9f0a8d4129f455305ee3da763782882b05c421a
santiaago <santiago.ariassar@gmail.com>
2015-05-28 00:44:36 +0100

    first call to golang backend from dart angular2 frontend

--- app/web/main.dart
+++ app/web/main.dart
@@ -1,4 +1,6 @@
+import 'dart:html';
 import 'package:angular2/angular2.dart';
+import 'dart:convert';
 
 // These imports will go away soon:
 import 'package:angular2/src/reflection/reflection.dart' show reflector;
@@ -8,11 +10,30 @@ import 'package:angular2/src/reflection/reflection_capabilities.dart' show Refle
   selector: 'my-app'
 )
 @View(
-  template: '<h1>Hello {{ name }} in dart</h1>'
+  template: '<h1>"{{hello.message}}"</h1>'
 )
+
 class AppComponent {
-  String name = 'Bob';
+  Hello hello = new Hello();
 }
+
+
+class Hello{
+
+  String message;
+
+  Hello(){
+        HttpRequest.getString('/api/hello')
+            .then((String content) {
+              Map parsedMap = JSON.decode(content);
+              message = parsedMap["Message"];
+            })
+            .catchError((Error error) {
+              print(error.toString());
+            });
+  }
+}
+
 
 main() {
   // Temporarily needed.
