6eca23ec5936a7923438de14b7ae30da78d895c7
santiaago <santiago.ariassar@gmail.com>
2015-06-13 16:13:49 +0100

    add start clock functionality

--- app/web/clock.dart
+++ app/web/clock.dart
@@ -1,17 +1,37 @@
 library clock;
 
 import 'dart:html';
+import 'dart:async';
+
 import 'package:angular2/angular2.dart';
 
 @Component(selector: 'clock')
 
-@View(template: '''<p>Clock: <i>{{ message }}</i></p>''')
+@View(template: '''
+<p>Clock: <i>{{ counter }}</i></p>
+<button (click)="start()" id="startButton">Start</button>
+''')
 
 class Clock{
+  @observable String counter='00:00';
+  Stopwatch watch = new Stopwatch();
+  Timer timer;
+  
+  void start() {
+    watch.start();
+    var oneSecond = new Duration(seconds:1);
+    timer = new Timer.periodic(oneSecond, updateTime);
+  }
 
-  String message;
-
-  Clock(){
-    message = "clock here";
+  void updateTime(Timer _) {
+    var s = watch.elapsedMilliseconds~/1000;
+    var m = 0;
+    
+    // The operator ~/ divides and returns an integer.
+    if (s >= 60) { m = s ~/ 60; s = s % 60; }
+    
+    String minute = (m <= 9) ? '0$m' : '$m';
+    String second = (s <= 9) ? '0$s' : '$s';
+    counter = '$minute:$second';
   }
 }
